ac95fcde4796076b573030a7ef712ccdf1b2819a
Ninh Dang <Ninhduy2000@gmail.com>
2024-01-27 16:38:37 +0700

    updateInstruction

--- instruction.py
+++ instruction.py
@@ -18,7 +18,7 @@ class Instruction:
       INSTRUCTION_HEAD,
       (SCREEN_WIDTH//2, 120),
       COLORS["instruction"],
-      font=FONTS["big"],
+      font=FONTS["medium"],
       shadow=True,
       shadow_color=(255,255,255),
       pos_mode="center"
@@ -29,19 +29,17 @@ class Instruction:
       CONTENT1,
       (SCREEN_WIDTH//2, 210),
       COLORS["instruction"],
-      font=FONTS["big"],
+      font=FONTS["medium"],
       shadow=True,
       shadow_color=(255,255,255),
       pos_mode="center"
     )
-
-
     ui.draw_text(
       self.surface,
       CONTENT2,
       (SCREEN_WIDTH//2, 300),
       COLORS["instruction"],
-      font=FONTS["big"],
+      font=FONTS["medium"],
       shadow=True,
       shadow_color=(255,255,255),
       pos_mode="center"
@@ -51,7 +49,7 @@ class Instruction:
       CONTENT3,
       (SCREEN_WIDTH//2, 390),
       COLORS["instruction"],
-      font=FONTS["big"],
+      font=FONTS["medium"],
       shadow=True,
       shadow_color=(255,255,255),
       pos_mode="center"
@@ -61,7 +59,7 @@ class Instruction:
       CONTENT4,
       (SCREEN_WIDTH//2, 480),
       COLORS["instruction"],
-      font=FONTS["big"],
+      font=FONTS["medium"],
       shadow=True,
       shadow_color=(255,255,255),
       pos_mode="center"
@@ -71,7 +69,7 @@ class Instruction:
       CONTENT6,
       (SCREEN_WIDTH//2, 660),
       COLORS["instruction"],
-      font=FONTS["big"],
+      font=FONTS["medium"],
       shadow=True,
       shadow_color=(255,255,255),
       pos_mode="center"
@@ -81,12 +79,11 @@ class Instruction:
       CONTENT7,
       (SCREEN_WIDTH//2, 750),
       COLORS["instruction"],
-      font=FONTS["big"],
+      font=FONTS["medium"],
       shadow=True,
       shadow_color=(255,255,255),
       pos_mode="center"
     )
-    
 
   def update(self):
     self.draw()
